38fe1c3e41c003980bdd7ac81228cdfc26b0543f
Jacob Mazelin <jjmazelin@gmail.com>
2025-09-29 13:35:53 -0400

    Wall Follower Working

--- p1_wall_follower/5_wall_follower.cpp
+++ p1_wall_follower/5_wall_follower.cpp
@@ -36,7 +36,7 @@ int main(int argc, const char *argv[])
     float setpoint = 0.325;  // The goal distance from the wall in meters
     float tolerance = 0.1;
     float scaling =  0.3; 
-    float kP = -1;
+    float kP = -0.5;
     float maxVelocity = 0.4;
 
     while (true) {
@@ -58,13 +58,13 @@ int main(int argc, const char *argv[])
             velocity = velocity;
         }
 
-        std::vector<float> v_to_wall = rayConversionCartisean(velocity, angle_to_wall);
+        std::vector<float> v_to_wall = rayConversionCartisean(0.5, angle_to_wall);
         std::vector<float> v_up = {0,0,1};
 
         std::vector<float> v_forward = crossProduct(v_to_wall, v_up);
-        std::vector<float>  correction_vector = //TODO
-        std::vector<float> final_command = v_forward + correction_vector;
-        robot.drive(v_forward[0], v_forward[1], 0);
+        std::vector<float> correction_vector = rayConversionCartisean(velocity, angle_to_wall);
+        std::vector<float> final_command = {v_forward[0] + correction_vector[0], v_forward[1] + correction_vector[1]};
+        robot.drive(final_command[0], final_command[1], 0);
 
         // *** End student code *** //
 
